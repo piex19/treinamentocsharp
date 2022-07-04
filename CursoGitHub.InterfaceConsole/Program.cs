99f7813d9a8c8a5edc3fcbd6f31f158ceb45dbc2
Pietra Paz <pietra.pinheiro@aluno.senai.br>
2022-07-04 10:46:42 -0300

    iniciando csharp

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace CursoGitHub.InterfaceConsole 
+{ // inicio do namespace
+    internal class Program
+    { // inicio da classe
+        static void Main(string[] args)
+        { // inicio do metodo (inicia-se com a letra maiuscula)
+            Console.WriteLine("Alô Mundo!");
+            Console.ReadKey();
+        }
+    }
+}
